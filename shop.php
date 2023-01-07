fde67e52b00c25a736a4e5739f30ac3b3f2fca3f
unknown <bra2aesam1994@gmail.com>
2023-01-08 00:16:07 +0300

    braa

--- shop.php
+++ shop.php
@@ -37,7 +37,7 @@ if(isset($_SESSION['user_id'])){
 
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="./css/style.css">
+   <link rel="stylesheet" href="css/style.css">
    
 </head>
 
